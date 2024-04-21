bfd9313d3851b68a8e17d67374f11fdedfba404e
p6542021@gmail.com <p65420201@gmail.com>
2024-04-21 03:31:17 -0400

    update

--- src/evaluate/grad_cam.py
+++ src/evaluate/grad_cam.py
@@ -9,6 +9,18 @@ import numpy as np
 import cv2
 import pytorch_grad_cam
 
+from torchinfo import summary
+
+from pytorch_grad_cam import GradCAM
+from pytorch_grad_cam.utils.image import show_cam_on_image, preprocess_image
+from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
+import sys
+import sys
+sys.path.insert(0, './src')
+from models.locnet import UNetWithResnet50Encoder
+
+#from ..models.locnet import UNetWithResnet50Encoder
+from torchinfo import summary
 from pytorch_grad_cam import GradCAM
 from pytorch_grad_cam.utils.image import show_cam_on_image, preprocess_image
 from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
@@ -18,7 +30,21 @@ from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
 ## Two functions to display the image and the heatmap for both LOCNet and ResNet50
 
 image_path = "./Image/dog/n02109525_37.JPEG"  # set to one image
-model_path = "./pre_trained/tmp.pth"  # set to the LocNet model path
+model_path = "./pre_trained/loc_model_best.pth.tar"  # set to the LocNet model path
+
+model_path2 = "./pre_trained/loc_model_best.pth"  # test 
+
+class WrapperModel(torch.nn.Module):
+    def __init__(self, model): 
+        super(WrapperModel, self).__init__()
+        self.model = model
+        
+    def forward(self, x):
+        # In out case conv_block_1 or conv_block_2
+        main_output, cls_pred, output_feature_map = self.model(x)
+
+        return output_feature_map.up_blocks[-1].conv_block_1
+    
 
 # Temp just for loading the dictionary
 
@@ -45,12 +71,33 @@ def TestOnOriginalImage(image_path):
     image = np.array(Image.open(image_path))
     return image/ 255
 
+
 def LoadLocNetModel():
-    # model = locnet()
-    # checkpoint = torch.load(model_path)
-    # model.load_state_dict(checkpoint)
+
+    checkpoint = torch.load(model_path,map_location='cpu')
+
+    model = UNetWithResnet50Encoder()
+    #model = torchvision.models.resnet50(pretrained= True)  
+    if 'state_dict' in checkpoint:
+        state_dict = checkpoint['state_dict']
+
+        # Sometimes the state_dict keys have a prefix like 'module.' if they were saved from a DataParallel module
+        # If so, you need to strip this prefix
+        state_dict = {k.replace('module.', ''): v for k, v in state_dict.items()}
+
+        # Load the stripped state_dict
+        model.load_state_dict(state_dict)
+    else:
+        # Directly try loading the checkpoint (not recommended if the above keys are present)
+        model.load_state_dict(checkpoint)
+    #summary(model)
+    # first_layer_name = next(model.named_children())[0]
+    # print("First layer name:", first_layer_name)
+    # for name, layer in model.named_children():
+    #     print(name, layer)
     
-    model = torch.load(model_path)
+    # layer = model.up_blocks[-1]
+    # print(layer)
     return model
 
 # Current set to just display one, change the file path to the image you want to display
@@ -59,9 +106,10 @@ def ResNet50Cam():
     target_layer = [model.layer4[-1] ]
     img = TestOnOriginalImage(image_path)
     input_tensor_temp = TestOnOneImage(image_path)
-    target = None ## Current None
+    target = [ClassifierOutputTarget(2)] ## index of the class, seems 2 for the sample
+    #target = None
     with GradCAM(model=model, target_layers=target_layer) as cam:
-        grayscale_cams = cam(input_tensor=input_tensor_temp, targets=None)
+        grayscale_cams = cam(input_tensor=input_tensor_temp, targets=target)
         cam_image = show_cam_on_image(img, grayscale_cams[0, :], use_rgb=True)
     cam = np.uint8(255*grayscale_cams[0, :])
     cam = cv2.merge([cam, cam, cam])
@@ -72,23 +120,26 @@ def ResNet50Cam():
 
 # check the target layer set to before the bridge of U-Net
 def LocNetCam():
-    model = LoadLocNetModel()
-    
-    target_layer = [model.layer4[-1] ]
+    #model = LoadLocNetModel()
+    model = WrapperModel(LoadLocNetModel())
+    target_layer = [model] # alreayd wrapped
     img = TestOnOriginalImage(image_path)
     input_tensor_temp = TestOnOneImage(image_path)
-    target = None ## Current None
+    target = [ClassifierOutputTarget(2)] 
+
     with GradCAM(model=model, target_layers=target_layer) as cam:
-        grayscale_cams = cam(input_tensor=input_tensor_temp, targets=None)
+        grayscale_cams = cam(input_tensor=input_tensor_temp, targets=target)
         cam_image = show_cam_on_image(img, grayscale_cams[0, :], use_rgb=True)
     cam = np.uint8(255*grayscale_cams[0, :])
     cam = cv2.merge([cam, cam, cam])
     images = np.hstack((np.uint8(255*img) , cam_image))
+
     Image.fromarray(images)
     plt.imshow(images)
     plt.show()
     
     
-ResNet50Cam()
+#ResNet50Cam()
 LocNetCam()
-    
+#LoadLocNetModel()
+
