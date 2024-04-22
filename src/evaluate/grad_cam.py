679f7b9d202623f2cbb41748c1ea80ba6d4aff34
p6542021@gmail.com <p65420201@gmail.com>
2024-04-21 23:15:36 -0400

    temp version for workable locnet cam

--- src/evaluate/grad_cam.py
+++ src/evaluate/grad_cam.py
@@ -38,12 +38,14 @@ class WrapperModel(torch.nn.Module):
     def __init__(self, model): 
         super(WrapperModel, self).__init__()
         self.model = model
+        self.input_block = model.input_block
+        self.down_blocks = model.down_blocks
         
     def forward(self, x):
         # In out case conv_block_1 or conv_block_2
-        main_output, cls_pred = self.model(x)
-
-        return cls_pred
+        #main_output, cls_pred , output_feauture = self.model(x, with_output_feature_map=True)
+        outputs = self.model(x)
+        return outputs[1]
     
 
 # Temp just for loading the dictionary
@@ -61,6 +63,7 @@ def NormalProcess(image_path):
     input_tensor = preprocess_image(rgb_img,
                                 mean=[0.485, 0.456, 0.406],
                                 std=[0.229, 0.224, 0.225])
+    print(input_tensor.shape)
     return input_tensor
 
 def TestOnOneImage(image_path):
@@ -77,11 +80,13 @@ def TestOnOneImage(image_path):
     
     img = Image.open(image_path)
     img = trans(img)
-    input_tensor = preprocess_image(img,
-                                mean=[0.485, 0.456, 0.406],
-                                std=[0.229, 0.224, 0.225])
-    
-    return input_tensor
+    # input_tensor = preprocess_image(img,
+    #                             mean=[0.485, 0.456, 0.406],
+    #                             std=[0.229, 0.224, 0.225])
+    img = torchvision.transforms.ToTensor()(img)
+    img= img.reshape(1, 3, 224, 224)
+    print(img.shape)
+    return img
 
 def TestOnOriginalImage(image_path):
     image = np.array(Image.open(image_path))
@@ -93,6 +98,7 @@ def LoadLocNetModel():
     checkpoint = torch.load(model_path,map_location='cpu')
 
     model = UNetWithResnet50Encoder()
+    
     #model = torchvision.models.resnet50(pretrained= True)  
     if 'state_dict' in checkpoint:
         state_dict = checkpoint['state_dict']
@@ -107,6 +113,8 @@ def LoadLocNetModel():
         # Directly try loading the checkpoint (not recommended if the above keys are present)
         model.load_state_dict(checkpoint)
     #summary(model)
+    
+    #print(model.down_blocks[0])
     # first_layer_name = next(model.named_children())[0]
     # print("First layer name:", first_layer_name)
     # for name, layer in model.named_children():
@@ -134,33 +142,44 @@ def ResNet50Cam():
     plt.imshow(images)
     plt.show()
 
+def visualize_cam(heatmap):
+    plt.imshow(heatmap, cmap='jet', interpolation='bilinear')
+    plt.colorbar()
+    plt.show()
+    
 # check the target layer set to before the bridge of U-Net
 def LocNetCam():
     #model = LoadLocNetModel()
-    # model = WrapperModel(LoadLocNetModel())
-    # target_layer = [model] # alreayd wrapped
+    #model.eval()
     model = WrapperModel(LoadLocNetModel())
-    target_layer = [model]
+    # target_layer = [model] # alreayd wrapped
+    #model = WrapperModel(LoadLocNetModel())
+    
+    target_layer = [model.down_blocks[0] ]
     img = TestOnOriginalImage(image_path)
     input_tensor_temp = TestOnOneImage(image_path)
+    #input_temp2 = NormalProcess(image_path)
     target = [ClassifierOutputTarget(2)] 
 
-
-    with GradCAM(model=model, target_layers=target_layer) as cam:
-        grayscale_cams = cam(input_tensor=input_tensor_temp, targets=None)
-        cam_image = show_cam_on_image(img, grayscale_cams[0, :], use_rgb=True)
+    # with GradCAM(model=model, target_layers=target_layer) as cam:
+    #     grayscale_cams = cam(input_tensor=input_tensor_temp, targets=target)
+    #     cam_image = show_cam_on_image(img, grayscale_cams[0, :], use_rgb=True)
 
     
-    cam = np.uint8(255*grayscale_cams[0, :])
-    cam = cv2.merge([cam, cam, cam])
-    images = np.hstack((np.uint8(255*img) , cam_image))
-
-    Image.fromarray(images)
-    plt.imshow(images)
-    plt.show()
+    # cam = np.uint8(255*grayscale_cams[0, :])
+    # cam = cv2.merge([cam, cam, cam])
+    # images = np.hstack((np.uint8(255*img) , cam_image))
+    cam = GradCAM(model=model, target_layers=target_layer)
+    grayscale_cams = cam(input_tensor=input_tensor_temp , targets=target)
+    print("Grayscale CAMs:", grayscale_cams.shape)
+    heatmap = grayscale_cams[0]  # Simplify to (224, 224)
+    visualize_cam(heatmap)
+    # Image.fromarray(images)
+    # plt.imshow(images)
+    # plt.show()
     
-    
-ResNet50Cam()
-#LocNetCam()
-#LoadLocNetModel()
-
+#ResNet50Cam()
+# LocNetCam()
+# LocNetCam()
+# LoadLocNetModel()
+LocNetCam()
