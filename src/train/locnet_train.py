ab6f614178d5750fa6cc059ffb9cf3f1fcf203a1
Leon-Leyang <leyang_hu@brown.edu>
2024-04-28 19:54:11 -0400

    Init `n_cycle`

--- src/train/locnet_train.py
+++ src/train/locnet_train.py
@@ -368,6 +368,7 @@ def main():
     num_training_steps_per_epoch = len(loader_train)
     annealing_steps = num_training_steps_per_epoch * (args.epochs - args.rest_cce) + 2
 
+    n_cycle = None
     if args.loss_anneal:
         n_cycle = 5
         # import ipdb;ipdb.set_trace()
